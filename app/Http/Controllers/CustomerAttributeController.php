59669face9536c64ab89e037f264c480538018fe
Tanveer Raza <mniraza2@gmail.com>
2025-12-16 18:25:58 +0500

    Update CustomerAttributeController.php

--- app/Http/Controllers/CustomerAttributeController.php
+++ app/Http/Controllers/CustomerAttributeController.php
@@ -28,6 +28,8 @@ class CustomerAttributeController extends Controller
     }
 
     /**
+     * Display the form for creating a new resource.
+     */
      * Show the form for creating a new resource.
      */
     public function create()
