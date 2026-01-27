4ec6fffc91cc73d6a290530418504234052673b9
Tanveer Raza <mniraza2@gmail.com>
2026-01-27 11:12:15 +0500

    Update BrandWiseSalesReportController.php

--- app/Http/Controllers/BrandWiseSalesReportController.php
+++ app/Http/Controllers/BrandWiseSalesReportController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
 
-
 class BrandWiseSalesReportController extends Controller
 {
     public function index(Request $request)
